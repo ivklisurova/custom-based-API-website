adbed31559ce472a0485c7f57e66af17763be7e2
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2022-01-16 23:36:16 +0200

    add about html

--- main.py
+++ main.py
@@ -17,6 +17,11 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 @app.route('/test')
 def test():
     return 'Works'
