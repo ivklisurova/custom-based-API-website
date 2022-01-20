3fb1d43b0fdfb6303fa6c27d61842c8de5829695
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2022-01-20 21:04:39 +0200

    add api request function to OMDB

--- api.py
+++ api.py
@@ -1,15 +1,20 @@
 import requests
+from settings import OMDBb_API_KEY, OMDb_LINK
+from settings import db
+from models import Movie
 
-OMDBb_API_KEY = '8d593346'
-OMDb_LINK = 'http://www.omdbapi.com/'
 
-params = {
-    'apikey': [OMDBb_API_KEY],
-    't': 'The Godfather',
+def add_movie(movie_name, user_id):
+    params = {
+        'apikey': [OMDBb_API_KEY],
+        't': f'{movie_name}'
 
-}
+    }
 
-response = requests.get(url=OMDb_LINK, params=params)
-response.raise_for_status()
-data = response.json()
-print(data)
+    response = requests.get(url=OMDb_LINK, params=params)
+    response.raise_for_status()
+    data = response.json()
+    new_movie = Movie(title=movie_name, year=int(data['Year']), description=data['Plot'],
+        ranking=float(data['Ratings'][0]['Value'].split('/')[0]), img_url=data['Poster'], user_id=user_id)
+    db.session.add(new_movie)
+    db.session.commit()
