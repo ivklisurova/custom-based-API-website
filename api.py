f51d2ccd88cc5615cb36a3286fe158b968f8d346
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2022-01-24 19:38:34 +0200

    update search movie settings

--- api.py
+++ api.py
@@ -14,7 +14,7 @@ def add_movie(movie_name, user_id):
     response = requests.get(url=OMDb_LINK, params=params)
     response.raise_for_status()
     data = response.json()
-    new_movie = Movie(title=movie_name, year=int(data['Year']), description=data['Plot'],
+    new_movie = Movie(title=movie_name, year=data['Year'], description=data['Plot'],
         ranking=float(data['Ratings'][0]['Value'].split('/')[0]), img_url=data['Poster'], user_id=user_id)
     db.session.add(new_movie)
     db.session.commit()
